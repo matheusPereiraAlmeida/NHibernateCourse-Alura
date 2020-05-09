d0d7d397559b27d455fda0e26ae4a17f6277c636
Matheus Pereira de Almeida <matheus.pereira.almeida10@gmail.com>
2020-05-09 00:36:20 -0300

    First commit

---
+++ loja/loja/Program.cs
@@ -0,0 +1,25 @@
+﻿using loja.dao;
+using loja.Entidades;
+using loja.Info;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+
+namespace loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISession session = NHibernateHelper.AbreSession();
+            UsuarioDao usuario = new UsuarioDao(session);
+
+            Usuario novoUsuario = new Usuario();
+            novoUsuario.Nome = "Patricia";
+            usuario.Adiciona(novoUsuario);
+            session.Close();
+
+            Console.Read();
+        }
+    }
+}
---
+++ loja/loja/dao/UsuarioDao.cs
@@ -0,0 +1,33 @@
+﻿using loja.Entidades;
+using loja.Info;
+using MySqlX.XDevAPI;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loja.dao
+{
+    public class UsuarioDao
+    {
+        private ISession session;
+
+        public UsuarioDao(ISession session)
+        {
+            this.session = session;
+        }
+        public void Adiciona(Usuario usuario)
+        {
+            ITransaction transation = session.BeginTransaction();
+            session.Save(usuario);
+            transation.Commit();
+        }
+
+        public Usuario BuscaPorId(int id)
+        {
+            return session.Get<Usuario>(id);
+        }
+    }
+}
